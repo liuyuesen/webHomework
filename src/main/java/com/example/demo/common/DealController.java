14cbf6521b2d79fd1451d78f3b65857c92884817
LITTLEEEEE <912089196@qq.com>
2018-12-16 22:28:24 +0800

    111

--- src/main/java/com/example/demo/common/DealController.java
+++ src/main/java/com/example/demo/common/DealController.java
@@ -21,11 +21,14 @@ public class DealController {
         boolean flag = false;
         //leave or out
         String loro = null;
-        if(function==0){
+        if(function==1){
             loro="leave_form";
+        }else if(function==0){
+            loro="out_form";
         }
         try {
             String sql1="select status from "+loro+" where id = "+did+";";
+            //System.out.print(sql1);
             DBManager dbManager = new DBManager(sql1);
             ResultSet result;
             result = dbManager.preparedStatement.executeQuery();
@@ -34,34 +37,38 @@ public class DealController {
                 pre_status=result.getString("status");
             }
             String st = null,opinion1=null,opinion2=null,opinion3=null;
-            if(admission.equals(0)&&status=="department_manager"){
+            if(admission.equals("0")&&status.equals("department_manager")){
                 st="4";//不同意按钮--对应状态4
                 opinion1=description;
-            }else if(admission.equals(0)&&status=="deputy_general_manager"){
+            }else if(admission.equals("0")&&status.equals("deputy_general_manager")){
                 st="4";
                 opinion2=description;
-            }else if(admission.equals(0)&&status=="general_manager"){
+            }else if(admission.equals("0")&&status.equals("general_manager")){
                 st="4";
                 opinion3=description;
-            }else if(admission.equals(1)&&status=="department_manager"){
+            }else if(admission.equals("1")&&status.equals("department_manager")){
                 st="1";
-            }else if(admission.equals(1)&&status=="deputy_general_manager"){
+            }else if(admission.equals("1")&&status.equals("deputy_general_manager")){
                 st="2";
-            }else if(admission.equals(1)&&status=="general_manager"){
+            }else if(admission.equals("1")&&status.equals("general_manager")){
                 st="3";
             }
             //-----------下边是做第一次更改--------------------------------------
-            String sql="update "+loro+" set status = "+st+" , pre_status = "+pre_status+" where id = "+did+";";
+            String sql="update "+loro+" set status = "+st+" , pre_statu = "+pre_status+",opinion1=" +opinion1+
+                    " ,opinion2= "+opinion2+",opinion3="+opinion3+" where id = "+did+";";
+            System.out.print(sql);
             DBManager dbManager1 = new DBManager(sql);
+            dbManager1.preparedStatement.executeUpdate();
             //------------第一次改完了！！！！！！！！！！！！！！！！！！！！
             //------------现在我们来改第二次！！！！！！！！！！！！！！
-            String sql2="select status from "+loro+" where id = "+did+";";
-            DBManager dbManager2 = new DBManager(sql1);
+            String sql2="select status,stime,etime from "+loro+" where id = "+did+";";
+            //System.out.print();
+            DBManager dbManager2 = new DBManager(sql2);
             ResultSet result1;
-            result1 = dbManager.preparedStatement.executeQuery();
+            result1 = dbManager2.preparedStatement.executeQuery();
             String pre=null;
             boolean bi3 = false;
-            while(result.next()){
+            while(result1.next()){
                 pre=result1.getString("status");
                 Date date1 = result1.getDate("stime");
                 Date date2 = result1.getDate("etime");
@@ -70,12 +77,15 @@ public class DealController {
                     bi3=true;
                 }
             }
-            String thistate=null;
+
             if(pre.equals("3")||(pre.equals("2")&&(!bi3))){
-                String sqlfinal = "update "+loro+" set status = 5, pre_status = "+pre+" where id = "+did+";";
+                String sqlfinal = "update "+loro+" set status = 5, pre_statu = "+pre+" where id = "+did+";";
                 DBManager dbManager3 = new DBManager(sqlfinal);
+                dbManager3.preparedStatement.executeUpdate();
                 dbManager3.close();
+                System.out.print(sqlfinal);
             }
+            flag=true;
             dbManager.close();
             dbManager1.close();
             dbManager2.close();
@@ -83,7 +93,8 @@ public class DealController {
 
             result.close();
             result1.close();
-            flag=true;
+
+
         } catch (Exception e) {
             System.out.print(e);
         }
